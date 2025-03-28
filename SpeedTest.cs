2465564c4ba34b100a9d3c97374a30ba802bd969
Charlie B <charlie@charliebatten.co.uk>
2025-03-28 13:32:01 +0000

    Show output in console

--- SpeedTest.cs
+++ SpeedTest.cs
@@ -2,6 +2,8 @@ static class SpeedTest
 {
     public static string Run()
     {
+        Console.WriteLine("Running speedtest..");
+
         var cliOutput = "";
 
         using (var speedtestCli = new System.Diagnostics.Process())
@@ -17,6 +19,12 @@ static class SpeedTest
 
             speedtestCli.Close();
         }
+
+        var results = System.Text.Json.JsonSerializer.Deserialize<SpeedTestResults>(cliOutput);
+
+        var bytesToMbps = (float)8 / 1000 / 1000;
+
+        Console.WriteLine($"Server: {results.server.name} - Download: {results.download.bandwidth * bytesToMbps}Mbps - Upload: {results.upload.bandwidth * bytesToMbps}Mbps");
 
         return cliOutput;
     }
